cbbbcc50a877f01992f9b3a49fbb9820546457c6
mihermanogordo <andreo.olivella@gmail.com>
2026-04-19 18:41:59 +0200

    routes done

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { ProductDetail } from './pages/product-detail/product-detail';
+import { Cart } from './pages/cart/cart';
+import { Orders } from './pages/orders/orders';
+import { Login } from './pages/login/login';
+export const routes: Routes = [ { path: '', component: Home },
+  { path: 'products', component: Products },
+  { path: 'products/:id', component: ProductDetail },
+  { path: 'cart', component: Cart },
+  { path: 'orders', component: Orders },
+  { path: 'login', component: Login },];
