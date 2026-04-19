64b33e87fd47edaa8340b8af9c183689d51022db
mihermanogordo <andreo.olivella@gmail.com>
2026-04-19 13:16:50 +0200

    app routes no importa el @/angular/routes porque no tiene interface de la AppRoute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
-import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+
+export interface AppRoute {
+    path: string;
+    component: any;
+}
+
+export type Routes = AppRoute[];
+
 export const routes: Routes = [
     { path: '', component: Home }
 ];
